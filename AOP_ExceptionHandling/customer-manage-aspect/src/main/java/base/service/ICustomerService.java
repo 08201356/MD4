844ffd4af7118101a65f27fb52e029484e534e39
08201356 <nguyenhoainma0613@gmail.com>
2023-10-24 15:42:10 +0700

    add work

---
+++ AOP_ExceptionHandling/customer-manage-aspect/src/main/java/base/service/ICustomerService.java
@@ -0,0 +1,12 @@
+package base.service;
+
+import base.model.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICustomerService {
+    List<Customer> findAll() throws Exception;
+
+    Customer findOne(Long id) throws Exception;
+}
