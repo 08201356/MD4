0665a472d157bafdfe4f5918003fc0a85548ba76
08201356 <nguyenhoainma0613@gmail.com>
2023-12-14 15:00:27 +0700

    add work

---
+++ Authorization/spring-security-jwt/src/main/java/base/jwt/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package base.jwt.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
